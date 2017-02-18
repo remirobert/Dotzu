1ac9c806b679928def5c856950a7a492ccaa42f7
remi <remirobert33530@gmail.com>
2017-02-18 10:52:12 +0100

    update header

--- Dotzu/LoggerObjc.h
+++ Dotzu/LoggerObjc.h
@@ -9,7 +9,7 @@
 @import Foundation;
 
 #define NSLog(message, ...) [LoggerObjc verbose: [NSString stringWithFormat: message, ##__VA_ARGS__] file:[NSString stringWithUTF8String:__FILE__] function:[NSString stringWithUTF8String:__FUNCTION__] line:__LINE__]
-#define SBLogVerbose(message, ...) [LoggerObjc verbose: [NSString stringWithFormat: message, ##__VA_ARGS__] filePath:[NSString stringWithUTF8String:__FILE__] function:[NSString stringWithUTF8String:__FUNCTION__] line:__LINE__]
+#define LogVerbose(message, ...) [LoggerObjc verbose: [NSString stringWithFormat: message, ##__VA_ARGS__] filePath:[NSString stringWithUTF8String:__FILE__] function:[NSString stringWithUTF8String:__FUNCTION__] line:__LINE__]
 #define LogWarning(message, ...) [LoggerObjc warning: [NSString stringWithFormat: message, ##__VA_ARGS__] filePath:[NSString stringWithUTF8String:__FILE__] function:[NSString stringWithUTF8String:__FUNCTION__] line:__LINE__]
 #define LogInfo(message, ...) [LoggerObjc info: [NSString stringWithFormat: message, ##__VA_ARGS__] filePath:[NSString stringWithUTF8String:__FILE__] function:[NSString stringWithUTF8String:__FUNCTION__] line:__LINE__]
-#define SBLogError(message, ...) [LoggerObjc error: [NSString stringWithFormat: message, ##__VA_ARGS__] filePath:[NSString stringWithUTF8String:__FILE__] function:[NSString stringWithUTF8String:__FUNCTION__] line:__LINE__]
+#define LogError(message, ...) [LoggerObjc error: [NSString stringWithFormat: message, ##__VA_ARGS__] filePath:[NSString stringWithUTF8String:__FILE__] function:[NSString stringWithUTF8String:__FUNCTION__] line:__LINE__]
