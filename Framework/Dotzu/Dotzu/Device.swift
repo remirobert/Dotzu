5483a13ddd2d11b618a8a8b3f7777b5f8a4e31a0
Franco Meloni <franco.meloni91@gmail.com>
2017-06-07 14:02:59 -0700

    this should not be optional

--- Framework/Dotzu/Dotzu/Device.swift
+++ Framework/Dotzu/Dotzu/Device.swift
@@ -10,7 +10,7 @@ import UIKit
 
 struct Device {
     var osVersion: String?
-    var screenResolution: String?
+    var screenResolution: String!
     let deviceModel: DeviceModel = DeviceModel.current
     
     var getScreenSize: String?
